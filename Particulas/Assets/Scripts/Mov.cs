48272fe84af4b2719856dea68fc9f9b859b1f158
Zerhe <dante_ro@hotmail.com.ar>
2017-05-09 22:24:50 -0300

    Agregado helicoptero

---
+++ Particulas/Assets/Scripts/Mov.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov : MonoBehaviour {
+    Rigidbody rgb;
+    bool pressForwad;
+	void Start () {
+        rgb = GetComponent<Rigidbody>();
+        pressForwad = false;
+
+	}
+
+    void FixedUpdate()
+    {
+        if (pressForwad == true)
+        {
+            rgb.AddRelativeForce(-Vector3.forward, ForceMode.Impulse);
+        }
+    }
+    void Update () {
+		if (Input.GetKey(KeyCode.W))
+        {
+            pressForwad = true;
+        }
+	}
+}
